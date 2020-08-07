90b0cb6869660f094a2c99f8ac945000deefe60b
calapsu <sebaxtian098@hotmail.com>
2020-08-07 15:26:28 -0500

    my first commit

---
+++ tests/Feature/WalletTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Wallet;
+use App\Transfer;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+
+
+class WalletTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetWallet()
+    {
+        $wallet = factory(Wallet::class)->create();
+        $transfers = factory(Transfer::class, 3)->create([
+            'wallet_id' => $wallet->id
+        ]);
+
+        $response = $this->json('GET','/api/wallet');
+
+        $response->assertStatus(200)
+                ->assertJsonStructure([
+                    'id', 'money', 'transfers' => [
+                        '*' => [
+                            'id', 'amount', 'description', 'wallet_id'
+                        ]
+                    ]
+                ]);
+
+        $this->assertCount(3, $response->json()['transfers']);
+
+    }
+}
